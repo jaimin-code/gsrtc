6de76efb4b45da058634bf3c18ba7bcc9db0d8d2
jaimin-code <purohitjaimind@gmail.com>
2024-07-23 10:10:03 +0530

    frist commit

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo '<frameset rows="26%,*"> ' ;
-echo '<frame name ="f1" src="3657.png"  noresize" scrolling="no" border="no"> ';
-echo '<frameset cols="10%,90%"> ';
-echo '<frame name ="f2" src="menu.php" noresize border="no"> ';
-echo '<frame name ="f3"<image src="abcd.jpeg" height="400" width="1600"> </image>  >';
-echo ' </frameset>';
-echo '</frameset>';
-   ?>
